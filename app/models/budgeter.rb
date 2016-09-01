390005f43aedf4578de55d33940744c62769d7c6
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-01 13:50:10 -0700

    make active total in pennies for clarity

--- app/models/budgeter.rb
+++ app/models/budgeter.rb
@@ -6,7 +6,7 @@ class Budgeter < ActiveRecord::Base
 		self.items.select { |item| 	item.active }
 	end
 
-	def total_in_pennies
+	def active_total_in_pennies
 		active_totals = self.active_items.map do |item|
 			item.price_in_pennies
 		end
@@ -14,7 +14,7 @@ class Budgeter < ActiveRecord::Base
 	end
 
 	def pretty_total
-		"$#{((self.total_in_pennies.to_f) / 100).round(2)}"
+		"$#{((self.active_total_in_pennies.to_f) / 100).round(2)}"
 	end
 
 

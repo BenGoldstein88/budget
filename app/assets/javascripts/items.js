9b42eae1a0994b992ff1e0629d9e024c846c41cd
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-03 16:21:00 -0700

    tweaks

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
-
-		alert('hello')
-	$('#new_item_form').on('submit', function(event) {
-		
-		console.log("What'sup.")
-		event.preventDefault();
-
-
-	})
-
-
-
-});
 

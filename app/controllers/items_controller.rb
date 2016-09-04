6eef2744ad4bbef157646cc4a8ea0669c1dd7039
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-09-03 17:48:15 -0700

    fix CORS issues, allow requests from anyone anywhere!, rewrite item#create to accomodate decoupled react frontend

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,15 +21,30 @@ class ItemsController < ApplicationController
 	end
 
 	def create
-		item_params = params[:item]
-		@name = item_params[:name]
-		@description = item_params[:description]
-		@active = params[:active] || false
-		@price_in_pennies = params[:price_in_pennies]
-		budgeter = Budgeter.first
-		@budgeter_id = budgeter.id
-		@item = Item.create!(name: @name, description: @description, price_in_pennies: @price_in_pennies, active: @active, budgeter_id: @budgeter_id)
-		redirect_to root_path
+		# if request.xhr?
+			name = params[:name]
+			description = params[:description]
+			price_in_pennies = params[:price].to_i
+			budgeter_id = Budgeter.first.id
+			p name
+			p description
+			p price_in_pennies
+			p budgeter_id
+
+			Item.create!(name: name, description: description, price_in_pennies: price_in_pennies, budgeter_id: budgeter_id)
+			return nil
+
+		# else
+		# 	item_params = params[:item]
+		# 	@name = item_params[:name]
+		# 	@description = item_params[:description]
+		# 	@active = params[:active] || false
+		# 	@price_in_pennies = params[:price_in_pennies]
+		# 	budgeter = Budgeter.first
+		# 	@budgeter_id = budgeter.id
+		# 	@item = Item.create!(name: @name, description: @description, price_in_pennies: @price_in_pennies, active: @active, budgeter_id: @budgeter_id)
+		# 	redirect_to root_path
+		# end
 	end
 
 	def destroy
